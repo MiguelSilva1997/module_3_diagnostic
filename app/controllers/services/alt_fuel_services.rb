06a0113ef715db8b9827bb51e866cd5a205bc262
Miguel Silva <msilva0897@gmail.com>
2017-12-13 10:05:11 -0700

    trying to make first succesful call

--- app/controllers/services/alt_fuel_services.rb
+++ app/controllers/services/alt_fuel_services.rb
@@ -6,6 +6,13 @@ class AltFuelServices
       faraday.headers["X-Api-key"] = ENV["ALT_FUEL"]
       faraday.adapter Faraday.default_adapter
     end
+
+    def get_stations
+      response = conn.get("/api/alt-fuel-stations/v1/nearest.format?limit=10&radius=6&fuel_type=ELEC,LPG&location=#{zip}")
+      raw_data = JSON.parse(response.body)
+      raw_data['fuel_stations']
+    end
+
   end
 
 
--- app/controllers/services/station.rb
+++ app/controllers/services/station.rb
@@ -1,17 +1,24 @@
 class Station
 
-  attr_reader :name, :address, :fuel_types, :distance, :access_times
-
   def initialize(data)
-    @name = data
-    @address = data
-    @fuel_types = data
-    @distance = data
-    @access_times = data
+    @name = data['station_name']
+    @address = data["street_address"]
+    @fuel_type = data["fuel_type_code"]
+    @distance = data["distance"]
+    @access_times = data["access_days_time"]
   end
 
-  def self.create_stations
-
+  def self.create_stations(zip)
+    stations = AltFuelServices.new(zip).get_stations
+    make_stations(stations)
   end
 
+  def self.make_stations(stations)
+    stations.map do |station|
+      Station.new(station)
+    end
+  end
+
+  private
+    attr_reader :name, :address, :fuel_type, :distance, :access_times
 end

5e8bb90536a75d7fa8805e9908d38e739dd98b12
Miguel Silva <msilva0897@gmail.com>
2017-12-13 10:28:03 -0700

    added a cassette

--- spec/features/user_can_search_for_certain_zip_code_spec.rb
+++ spec/features/user_can_search_for_certain_zip_code_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 describe "when a user visits /" do
   it "and it fills the form with 80203" do
-    visit '/'
+    VCR.use_cassette("80203") do
+      visit '/'
 
-    fill_in :q, with: '80203'
+      fill_in :q, with: '80203'
 
-    click_on "Locate"
+      click_on "Locate"
 
-    expect(current_path).to eq("/search")
-    expect(page).to have_content(10)
-    expect(page).to have_content("UDR")
-    expect(page).to have_content("800 Acoma St")
-    expect(page).to have_content("ELEC")
-    expect(page).to have_content("0.31422")
-    expect(page).to have_content("24 hours daily")
-
+      expect(current_path).to eq("/search")
+      expect(page).to have_content(10)
+      expect(page).to have_content("UDR")
+      expect(page).to have_content("800 Acoma St")
+      expect(page).to have_content("ELEC")
+      expect(page).to have_content("0.31422")
+      expect(page).to have_content("24 hours daily")
+    end
   end
 end
